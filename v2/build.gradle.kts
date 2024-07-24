93735ab5226267ebfd25e4b3dbb08fa5026b851b
Pavan Singh <127223852+prime0i0@users.noreply.github.com>
2024-07-24 20:42:48 +0530

    project - 5 ( Machine Learning Project )

---
+++ v2/build.gradle.kts
@@ -0,0 +1,8 @@
+buildscript {
+    val agp_version by extra("7.4.0")
+    val agp_version1 by extra("8.1.1")
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+}
